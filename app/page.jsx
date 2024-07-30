35453de138ce034fc7c741ddbfaba17181d5edd5
Chetan <joshic626@gmail.com>
2024-07-30 16:27:35 +0530

    adding image on left side

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,12 @@
 "use client";
+import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 export default function Home() {
   const [phone, setPhone] = useState("");
-  const [screen, setScreen] = useState("submitted");
+  const [screen, setScreen] = useState("mobile");
   const [otp, setOtp] = useState(1234);
   const [mobileErr, setMobileErr] = useState("");
   const videoRef = useRef(null);
@@ -41,11 +42,6 @@ export default function Home() {
     setSelfie(imageData);
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log("Selfie submitted:", selfie);
-  };
-
   useEffect(() => {
     if (screen == "personal") {
       startCamera();
@@ -55,142 +51,162 @@ export default function Home() {
     <main>
       <div className=" flex justify-center items-center h-screen">
         {screen === "mobile" && (
-          <div className="w-1/3 bg-gray-400 flex flex-col py-3 gap-2 rounded px-2 relative">
-            <input
-              placeholder="Mobile"
-              type="phone"
-              value={phone}
-              onChange={(e) => {
-                setPhone(e.target.value);
-              }}
-              className="text-black bg-gray-300 rounded px-1 py-3 "
+          <div className="w-1/3 bg-white flex   rounded  relative">
+            <Image
+              src={
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQO8AAxAr9ZNr9JCwn2QZ35rSWWuiayhh0ayQ&s"
+              }
+              alt="fja"
+              width={100}
+              height={100}
+              className="w-full h-full"
             />
-            <p className="text-red-500 absolute top-16 ">{mobileErr}</p>
-            <button
-              className="text-white bg-blue-500 mt-5  px-2 py-2 rounded"
-              onClick={() => {
-                if (phone.length < 10) {
-                  return setMobileErr("Please enter valid number");
-                }
-                setScreen("otp");
-              }}
-            >
-              Request Otp
-            </button>
+            <div className=" flex flex-col px-2 py-4 ">
+              <input
+                placeholder="Mobile"
+                type="phone"
+                value={phone}
+                onChange={(e) => {
+                  setPhone(e.target.value);
+                }}
+                className="text-black bg-gray-300 rounded px-1 py-3 "
+              />
+              <p className="text-red-500 absolute top-16 ">{mobileErr}</p>
+              <button
+                className="text-white bg-blue-500 mt-5  px-2 py-2 rounded"
+                onClick={() => {
+                  if (phone.length < 10) {
+                    return setMobileErr("Please enter valid number");
+                  }
+                  setScreen("otp");
+                }}
+              >
+                Request Otp
+              </button>
+            </div>
           </div>
         )}
 
         {screen === "otp" && (
-          <div className="w-1/3 bg-gray-400 flex flex-col py-3 gap-4 rounded px-2">
-            <input
-              placeholder="Mobile"
-              type="number"
-              value={otp}
-              onChange={(e) => {
-                setPhone(e.target.value);
-              }}
-              className="text-black bg-gray-300 rounded px-1 py-3"
+          <div className="w-1/2 bg-white flex  gap-4 rounded ">
+            <Image
+              src={
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQO8AAxAr9ZNr9JCwn2QZ35rSWWuiayhh0ayQ&s"
+              }
+              alt="fja"
+              width={100}
+              height={100}
+              className="w-full h-full"
             />
+            <div className="flex flex-col w-full py-4 h-full justify-center px-3 text-black">
+              <label className="px-1">OTP</label>
+              <input
+                placeholder="Mobile"
+                type="number"
+                value={otp}
+                onChange={(e) => {
+                  setPhone(e.target.value);
+                }}
+                className="text-black bg-gray-300 rounded px-1 py-3 border border-blue-100"
+              />
 
-            <button
-              className="text-white bg-blue-500 mt-5  px-2 py-2 rounded"
-              onClick={() => {
-                setScreen("personal");
-              }}
-            >
-              Submit
-            </button>
+              <button
+                className="text-white bg-blue-500 mt-5  px-2 py-2 rounded"
+                onClick={() => {
+                  setScreen("personal");
+                }}
+              >
+                Submit
+              </button>
+            </div>
           </div>
         )}
 
         {screen === "personal" && (
-          <div className="w-1/3 bg-gray-400 flex flex-col py-3 gap-4 rounded px-2">
-            {!selfie && (
-              <div>
+          <div className=" flex justify-between w-1/2">
+            <Image
+              src={
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQO8AAxAr9ZNr9JCwn2QZ35rSWWuiayhh0ayQ&s"
+              }
+              alt="fja"
+              width={100}
+              height={100}
+              className="w-full h-full"
+            />
+            <div className="bg-white flex flex-col py-3 gap-4 rounded px-2">
+              {!selfie && (
                 <div>
-                  <video
-                    ref={videoRef}
-                    width="320"
-                    height="240"
-                    autoPlay
-                    className="w-full"
-                  ></video>
-                  <canvas
-                    ref={canvasRef}
-                    width="320"
-                    height="240"
-                    style={{ display: "none" }}
-                    className="w-full"
-                  ></canvas>
-                </div>
+                  <div>
+                    <video
+                      ref={videoRef}
+                      width="320"
+                      height="240"
+                      autoPlay
+                      className="w-full"
+                    ></video>
+                    <canvas
+                      ref={canvasRef}
+                      width="320"
+                      height="240"
+                      style={{ display: "none" }}
+                      className="w-full"
+                    ></canvas>
+                  </div>
 
-                <button
-                  type="button"
-                  className="bg-green-500 p-2 rounded-md
+                  <button
+                    type="button"
+                    className="bg-green-500 p-2 rounded-md
                   w-full mt-4 "
-                  onClick={takeSelfie}
-                >
-                  Take Selfie
-                </button>
-              </div>
-            )}
+                    onClick={takeSelfie}
+                  >
+                    Take Selfie
+                  </button>
+                </div>
+              )}
 
-            {selfie && (
-              <div>
-                <img src={selfie} alt="Selfie" className="w-full " />
+              {selfie && (
+                <div>
+                  <img src={selfie} alt="Selfie" className="w-full " />
 
-                <p className="text-red-500">{selfieErr}</p>
-                <button
-                  type="submit"
-                  className="bg-blue-500 p-2 w-full mt-4 rounded-md"
-                  onClick={() => {
-                    if (!selfie) {
-                      return setSelfieErr("Please capture selfie");
-                    }
-                    setScreen("details");
-                  }}
-                >
-                  Submit
-                </button>
-              </div>
-            )}
-          </div>
-        )}
+                  <p className="text-red-500">{selfieErr}</p>
+                </div>
+              )}
 
-        {screen == "details" && (
-          <div className="w-1/3 bg-gray-400 flex flex-col py-3 gap-4 rounded px-2">
-            <DatePicker
-              selected={startDate}
-              onChange={(date) => setStartDate(date)}
-              dateFormat="yyyy/MM/dd"
-              className="w-full p-2 rounded-md text-black"
-              placeholderText="Select a date"
-            />
-            <input
-              type="text"
-              value={name}
-              className="text-black p-2 rounded-md w-full"
-              placeholder="Full name"
-              onChange={(e) => setName(e.target.value)}
-            />
-
-            <button
-              type="submit"
-              className="bg-blue-500 p-2 w-full mt-4 rounded-md"
-              onClick={() => {
-                console.log({
-                  selfie: selfie,
-                  name: name,
-                  date: startDate,
-                  phone: phone,
-                  otp: otp,
-                });
-                alert(name, startDate, phone, otp);
-                setScreen("submitted");
-              }}
-            >
-              Submit
-            </button>
+              <DatePicker
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
+                dateFormat="yyyy/MM/dd"
+                className="w-full p-2 rounded-md text-black border border-blue-100"
+                placeholderText="Select a date"
+              />
+              <input
+                type="text"
+                value={name}
+                className="text-black p-2 rounded-md w-full border border-blue-100"
+                placeholder="Full name"
+                onChange={(e) => setName(e.target.value)}
+              />
+              <button
+                type="submit"
+                className="bg-blue-500 p-2 w-full mt-4 rounded-md"
+                onClick={() => {
+                  if (!selfie) {
+                    return setSelfieErr("Please capture selfie");
+                  }
+                  console.log({
+                    selfie: selfie,
+                    name: name,
+                    date: startDate,
+                    phone: phone,
+                    otp: otp,
+                  });
+                  alert(name, startDate, phone, otp);
+                  setScreen("submitted");
+                }}
+              >
+                Submit
+              </button>
+            </div>
           </div>
         )}
 
